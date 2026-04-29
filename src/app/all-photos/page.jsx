3573d626a8c3170d5b20e3fcc74c2d250d59d932
tahaimage8 <tahaimage8@gmail.com>
2026-04-30 05:00:32 +0600

    Adding Filter Section2

--- src/app/all-photos/page.jsx
+++ src/app/all-photos/page.jsx
@@ -2,19 +2,29 @@ import Category from "@/compornents/Category";
 import PhotoCard from "@/compornents/PhotoCard";
 import React from "react";
 
-const AllPhotosPage = async ({searchParams}) => {
-  const [category] =await searchParams;
+const AllPhotosPage = async ({ searchParams }) => {
+  const category = searchParams?.category;
 
-  const res = await fetch("https://artiva-ai.vercel.app/data.json");
+  const res = await fetch("https://artiva-ai.vercel.app/data.json", {
+    cache: "no-store",
+  });
+
   const data = await res.json();
 
-  const filteredPhotos = category ?  Photos.filter(photo=> photo.category.toLowerCase()== category.toLowerCase()) : data
-  // console.log(data)
+  const filteredPhotos = category
+    ? data.filter(
+        (photo) =>
+          photo.category?.toLowerCase() === category.toLowerCase()
+      )
+    : data;
+
   return (
     <div>
       <h1 className="text-2xl font-bold m-4">All AI Creations</h1>
-      <Category/>
-      <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-5 ">
+
+      <Category />
+
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-5">
         {filteredPhotos.map((photo) => (
           <PhotoCard key={photo.id} photo={photo} />
         ))}
